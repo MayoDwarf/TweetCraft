6495c0190025872e2f8e94a94af0462631942dde
Kailan (TheKomputerKing) Blanks <kailan@komputerking.net>
2014-04-11 16:07:20 +0100

    Prepare to start.

---
+++ src/main/java/tweetcraft/tweetcraft/TweetCraft.java
@@ -0,0 +1,11 @@
+package tweetcraft.tweetcraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TweetCraft extends JavaPlugin {
+    
+    @Override
+    public void onEnable() {
+    }
+    
+}
